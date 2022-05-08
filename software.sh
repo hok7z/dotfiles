e89020ff415d41ad7244b6d4d3bd70d7a7321e77
hok7z <hok7zv@tutanota.com>
2022-05-08 21:19:57 +0300

    Modify software.sh

--- software.sh
+++ software.sh
@@ -7,37 +7,43 @@ fi
 
 cd ~
 
-sudo pacman -S xorg xorg-server xorg-xinit lxappearance papirus-icon-theme slim 
+# Base graphics
+sudo pacman -S xorg xorg-server xorg-xinit lxappearance papirus-icon-theme
 sudo pacman -S bspwm sxhkd
 
-sudo pacman -S polkit-gnome keepassxc neofetch nitrogen flameshot kitty bottom tmux qalc
-sudo pacman -S rofi libqalculate rofi-qalc
+# Shell utils
+sudo pacman -S mpv yt-dlp
+sudo pacman -S zsh bat lsd fzf 
 
+sudo pacman -S polkit-gnome keepassxc neofetch nitrogen flameshot kitty bottom tmux 
+sudo pacman -S rofi libqalculate xsel 
+
+sudo pacman -S tor && sudo systemctl enable --now tor
+
+# Python 
+sudo pacman -S ipython python python-pip
+pip3 install aiogram peewee requests
+
+# NeoVim
 sudo pacman -S vim neovim neovim-qt
+sudo pacman -S clang npm ninja
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
     ~/.local/share/nvim/site/pack/packer/start/packer.nvim 
+pip3 install black nvim
 
-sudo pacman -S ipython python python-pip
-
-sudo pacman -S mpv yt-dlp
-sudo pacman -S zsh bat lsd
-
-sudo pacman -S qemu virt-manager ebtables dnsmasq firewalld 
+# Virtual Machine tool
+sudo pacman -S qemu virt-manager ebtables dnsmasq firewalld nmap  
 sudo systemctl --now enable libvirtd
 sudo systemctl --now enable firewalld
 sudo sytemctl restart libvirtd
 
-sudo pacman -S clang npm xsel
-pip3 install aiogram peewee requests
-
-git clone https://aur.archlinux.org/paru.git 
-cd paru
-makepkg -si
+git clone https://aur.archlinux.org/paru.git && cd paru
+makepkg -si 
 cd 
 
-pip3 install black
-sudo npm i g neovim
+sudo pacman -S mtpfs gvfs-mtp gvfs-gphoto2
+paru -S jmtpfs
 
-paru -S librewolf-bin freetube-bin timeshift-bin kotatogram-desktop-bin 
+paru -S librewolf-bin timeshift-bin kotatogram-desktop-bin 
 paru -S polybar-git light-git picom-tryone-git betterlockscreen
 paru -S ttf-iosevka nordic-darker-theme  
